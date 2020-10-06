cb02262a4e535052340eeca6a97b12983145efdd
TabulaWeb <67907195+TabulaWeb@users.noreply.github.com>
2020-10-06 13:55:19 +0300

    cart

--- forward/shop/models.py
+++ forward/shop/models.py
@@ -1,8 +1,12 @@
 from django.db import models
+from django.urls import reverse
 
 class Category(models.Model):
     name = models.CharField(max_length=200, db_index=True)
     slug = models.SlugField(max_length=200, unique=True)
+
+    def get_absolute_url(self):
+        return reverse('shop:product_list_by_category', args=[self.slug])
 
     class Meta:
         ordering = ('name',)
@@ -11,6 +15,8 @@ class Category(models.Model):
 
     def __str__(self):
         return self.name
+
+    
 
 class Product(models.Model):
     category = models.ForeignKey(Category, related_name='products', on_delete=models.CASCADE)
@@ -26,6 +32,9 @@ class Product(models.Model):
     class Meta:
         ordering = ('name',)
         index_together = (('id', 'slug'),)
+
+    def get_absolute_url(self):
+        return reverse('shop:product_detail', args=[self.id, self.slug])
         
     def __str__(self):
         return self.name
